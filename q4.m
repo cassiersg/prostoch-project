d7c0b5d4feed2068dcb162504b14cda7ea41c549
Gaëtan Cassiers <gaetan.cassiers@gmail.com>
2016-04-23 22:49:11 +0200

    Extract plots q4 & q5 to separate script

--- q4.m
+++ q4.m
@@ -1,5 +1,5 @@
 load data.mat
-close all;
+
 k_max = 26;
 
 T = 1;
@@ -40,24 +40,6 @@ xt = [...
     Inf,...
     'SIR');
 
-target_velocities = diff([target, 2*target(:,end) - target(:,end-1)]')'/T;
 
-for t = [1, 2, 3, 15, 26]
-    figure('Name', ['k = ', num2str(t)]);
-    subplot(2, 1, 1);
-    title('positions');
-    hold on;
-    plot(...
-        xt_pre_resampling(1,:,t), xt_pre_resampling(2,:,t), '.',...
-        xt_post_resampling(1,:,t), xt_post_resampling(2,:,t), '.');
-    plot(observer(1,:), observer(2,:), 'x', target(1,:), target(2,:), 'x');
-	plot(xt_estimated(1,1:t), xt_estimated(2,1:t), 'd');
-    legend('pre resampling', 'post resampling', 'observer', 'target', 'estimated target');
-    subplot(2, 1, 2);
-    plot(...
-        xt_pre_resampling(3,:,t), xt_pre_resampling(4,:,t), '.',...
-        xt_post_resampling(3,:,t), xt_post_resampling(4,:,t), '.',...
-        target_velocities(1,t), target_velocities(2,t), 'd');
-    title('velocities');
-    legend('pre resampling', 'post resampling', 'true (almost)');
-end
+
+plots_q4;
