b91265ca5604250317ae1de0863b3fb3640bc21c
Gaëtan Cassiers <gaetan.cassiers@gmail.com>
2016-05-13 11:14:06 +0200

    Update fig q6 (and save data)

--- matlab/plot_q6.m
+++ matlab/plot_q6.m
@@ -7,6 +7,7 @@
 k_max = length(RMS);
 f = figure('Name','q6 : CRLB and RMS error considering relative position');
 plot(1:k_max,CRLBrms,'x-.',1:k_max,RMS,'x-.');
-legend({'CRLB','RMS error'}, 'Location', 'northwest');
+xlabel('step'); ylabel('RMS');
+legend({'CRLB','RMS error'}, 'Location', 'southwest');
 savefig(sprintf('q6'));
 close(f);
